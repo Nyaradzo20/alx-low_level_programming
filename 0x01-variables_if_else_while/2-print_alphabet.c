7262e07f1610700e239e12574c8ec381aab677c0
Nyaradzo20 <nchinyati20@gmail.com>
2022-03-13 14:47:47 -0700

    lowercase and putcar

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+*main - to print alphabet in lowercase
+* Return:(0)  unsigned chars
+*/
+int main(void)
+{
+char alphabe;
+for (alphabe = 'a'; alphabe <= 'z'; alphabe++)
+putchar(alphabe);
+putchar('\n');
+return (0);
+}
