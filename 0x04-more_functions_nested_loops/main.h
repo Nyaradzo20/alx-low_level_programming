0ddf10eeecc62220c1ce133e8c9eded49b388c5e
Nyaradzo20 <nchinyati20@gmail.com>
2022-03-18 04:23:10 -0700

    uppercase c?

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,42 @@
+#ifndef MAIN_H
+#define MAIN_H
+int _putchar(char c);
+/**
+ * _putchar - puts character to standard output
+ * @c: character to put to standard output
+ */
+
+int _isupper(int c);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#endif
