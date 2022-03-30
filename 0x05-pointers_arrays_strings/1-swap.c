1e595a35c30745109bc722d977532ce7ab158a0d
Nyaradzo20 <nchinyati20@gmail.com>
2022-03-30 13:49:29 -0700

    easier method

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -6,14 +6,11 @@
  *@b : second int
  *Return 0
  **/
-
 void swap_int(int *a, int *b)
 {
-int temp;
-/* Copy the value of num1 to some temp variable*/
-temp = *a;
-/*copy value of num 2 to num1*/
-*b = *a;
-/*cpy the value of num1 storred in temo to num2*/
-*b = temp;
+int c;
+c = *a;
+*b = c;
+*a = *b;
 }
+/*kungokanyanisa until dzese dzasangana*/
