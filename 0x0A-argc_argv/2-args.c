0c02796764e976158888c43c1d9450cc86dd7635
Nyaradzo20 <nchinyati20@gmail.com>
2022-04-05 15:24:40 -0700

    static

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -5,10 +5,10 @@
 * @argv: The argument vector
 * Return: Always 0
 */
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 int i;
-for(i =0; i < argc; i++)
+for (i = 0; i < argc; i++)
 printf("%s\n", argv[i]);
 return (0);
-}	       
+}
