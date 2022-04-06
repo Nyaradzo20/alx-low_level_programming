74c7a870d8724a2d20d6a412088ed05192b7f462
Nyaradzo20 <nchinyati20@gmail.com>
2022-04-06 07:25:45 -0700

    made a static librarygit add .!

---
+++ 0x09-static_libraries/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *_strlen - prints back the length of a string
+ *@s: the value to be evaluated
+ *Return: length of the string
+ */
+int _strlen(char *s)
+{
+  int length;
+  for (length = 0; s[length] != '\0'; length++);
+  return (length);
+}
---
+++ 0x09-static_libraries/6-abs.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _abs -should compute the absolute value of integer
+ * @n: int argument
+ * Return: absolute value
+ */
+int _abs(int n)
+{
+  if (n > 0)
+    return (n);
+  else if (n < 0)
+    return (-n);
+  return (0);
+}
