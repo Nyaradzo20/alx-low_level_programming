e62f42a5125ce36c6be9c4f6ab788fcca30c88da
Nyaradzo Chinyati <88329176+Nyaradzo20@users.noreply.github.com>
2022-04-06 23:28:50 +0200

    betty

--- 0x09-static_libraries/1-strncat.c
+++ 0x09-static_libraries/1-strncat.c
@@ -8,16 +8,16 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-  int i = 0;
-  int j = 0;
-  while (dest[i] != '\0') /*i is index of null terminator*/
-    i++;
-  while (src[j] != src[n]) /*append replacing null terminator*/
-    {
-      dest[i] = src[j];
-      i++;
-      j++;
-    }
-  dest[i] = '\0';
-  return (dest);
+int i = 0;
+int j = 0;
+while (dest[i] != '\0') /*i is index of null terminator*/
+i++;
+while (src[j] != src[n]) /*append replacing null terminator*/
+{
+dest[i] = src[j];
+i++;
+j++;
+}
+dest[i] = '\0';
+return (dest);
 }
