5fbc67026a0c341aa31a24f7d154ce06e30dbd86
Nyaradzo20 <nchinyati20@gmail.com>
2022-04-29 03:32:01 -0700

    i can do this for hours

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -2,6 +2,8 @@
 #define MAIN_H
 
 #include <stdio.h>
+int convert_day(int month, int day);
+void print_times_table(int n);
 int largest_number(int a, int b, int c);
   
 void positive_or_negative(int i);
