f872db2d93af66f916df84688a89724d13c9c755
Nyaradzo Chinyati <88329176+Nyaradzo20@users.noreply.github.com>
2022-05-06 23:58:38 +0200

    Update 3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -10,11 +10,12 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-int counter, cmpVal;
-while (s1[counter] == s2[counter] && s1[counter] != '\0')
+int compar;
+while(*s1 && *s2 != '\0')
 {
-counter++;
+s1++;
+s2++;
 }
-cmpVal = s1[counter] - s2[counter];
-return (cmpVal);
+compar = *s1 - *s2;
+return(compar);
 }
