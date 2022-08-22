f3fac1d51f23bfe0521e922fb6bb22f339af854e
Nyaradzo Chinyati <88329176+Nyaradzo20@users.noreply.github.com>
2022-08-22 23:37:21 +0200

    updated

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,46 +1,42 @@
 #include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
 
 /**
-* delete_nodeint_at_index - delete a node at a given position
-* @head: pointer to head pointer of linked list
-* @index: index to delete node
-* Return: 1 if succeeded, or -1 if failed
-*/
-
+ * delete_nodeint_at_index - A function that deletes the node at
+ * specified index from list
+ * @head: A pointer to listint_t structure
+ * @index: The index of the node to delete
+ * Return: 1 if succeeded, or -1 if it failed
+ */
 int delete_nodeint_at_index(listint_t **head, unsigned int index)
 {
+	listint_t *temp, *dnode;
+	unsigned int i = 1;
 
-unsigned int i = 0;
-listint_t *tmp, *tmp2;
+	if (*head == NULL)
+		return (-1);
 
-/* account for empty list */
-if (*head == NULL)
-return (-1);
-
-tmp = *head;
-
-/* account for deleting beginning node */
-if (index == 0)
-{
-*head = tmp->next;
-free(tmp);
-return (1);
+	temp = *head;
+	if (index == 0)
+	{
+		*head = (*head)->next;
+		free(temp);
+	}
+	else
+	{
+		while (i < index)
+		{
+			temp = temp->next;
+			if (temp == NULL)
+				return (-1);
+			i++;
+		}
+		dnode = temp;
+		dnode = dnode->next;
+		temp->next = dnode->next;
+		free(dnode);
+	}
+	return (1);
 }
 
-/* iterate tmp to idx prior to idx we want to delete */
-while (i < (index - 1) && tmp != NULL)
-{
-tmp = tmp->next;
-i++;
-}
-
-/* account for idx out of range: don't delete and return */
-if (i != (index - 1) || tmp->next == NULL)
-return (-1);
-
-/* link prior idx before delete */
-tmp2 = tmp->next;
-tmp->next = (tmp->next)->next;
-free(tmp2);
-
-return (1);
