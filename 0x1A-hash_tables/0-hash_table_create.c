8d146906272a9c82659a91b852f98e2d85f48b1c
Nyaradzo Chinyati <88329176+Nyaradzo20@users.noreply.github.com>
2022-12-17 22:05:49 +0200

    done

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,29 +1,34 @@
 #include "hash_tables.h"
+
 /**
-* hash_table_create - function
-* @size: size of the hash table array
-*
-* Description: Creates a hash table.
-* Return: Ptr to new table, or NULL if fail
-*/
+ * hash_table_create - A function that creates a hash table
+ * @size: The size of the array
+ *
+ * Return: If an error occurs - NULL
+ *         Otherwise - a pointer to the new hash table
+ */
+
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *new_table;
-	unsigned long int index;
+	hash_table_t *hash_table_created;
+	unsigned long int i;
 
-	if (size <= 0) /* size must be a positive number */
+	/* allocate space for the hash table */
+	hash_table_created = malloc(sizeof(hash_table_t));
+
+	if (hash_table_created == NULL)
 		return (NULL);
 
-/* allocate memory for table and array and check for fail */
-	new_table = malloc(sizeof(hash_table_t));
-	if (new_table == NULL)
+	hash_table_created->size = size;
+	hash_table_created->array = malloc(sizeof(hash_node_t *) * size);
+
+	if (hash_table_created->array == NULL)
 		return (NULL);
-	new_table->array = malloc(sizeof(hash_node_t *) * size);
-	if (new_table->array == NULL)
-		return (NULL);
-	new_table->size = size;
-/* traverse size and set each array index to null inside table */
-	for (index = 0; index < size; index++)
-		new_table->array[index] = NULL;
-	return (new_table);
+
+	for (i = 0; i < size; i++)
+	{
+		hash_table_created->array[i] = NULL;
+	}
+
+	return (hash_table_created);
 }
