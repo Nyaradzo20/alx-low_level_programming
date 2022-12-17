4729c2b83a6d47989e37bfb2ba2dc48c4cc6570e
Nyaradzo Chinyati <88329176+Nyaradzo20@users.noreply.github.com>
2022-12-17 21:59:24 +0200

    done

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,19 +1,20 @@
 #include "hash_tables.h"
 
 /**
-* hash_djb2 - implement djb2 algorithm (one of best string hash functions)
-* source code copied from http://www.cse.yorku.ca/~oz/hash.html
-*
-* @str: value
-* Return: key
-*/
+ * hash_djb2 - Hash function implementing the djb2 algorithm.
+ * @str: The string to hash.
+ *
+ * Return: The calculated hash.
+ */
+
 unsigned long int hash_djb2(const unsigned char *str)
 {
-unsigned long hash = 5381;
-int c;
+	unsigned long int hash;
+	int count;
 
-while ((c = *str++))
-hash = ((hash << 5) + hash) + c; 
+	hash = 5381;
+	while ((count = *str++))
+		hash = ((hash << 5) + hash) + count; /* hash * 33 + count */
 
-return (hash);
+	return (hash);
 }
